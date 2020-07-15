f51c9bbb91b85762e961e4841606d9271a86b522
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-07-15 22:44:08 +0000

    [Fix]: Typo

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -3,6 +3,6 @@
 
 
 def np_elementwise(mat1, mat2):
-    """Performs element-wise addition, subtraction, multiplication, and division
-    in a numpy array"""
+    """Performs element-wise addition, subtraction,
+    multiplication, and division in a numpy array"""
     return(mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
