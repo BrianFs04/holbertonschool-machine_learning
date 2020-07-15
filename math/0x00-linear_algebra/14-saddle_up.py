651b87529ede7fa9a60edd029b4a853a61acde8d
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-07-15 22:48:00 +0000

    [Feature]: Matrix multiplication with numpy

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function np_matmul"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication between numpy arrays"""
+    return(np.dot(mat1, mat2))
