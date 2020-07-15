df8fe2a6cca01f01877bc4fbe57c4dc941741835
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-07-15 22:25:32 +0000

    [Fix]: Concatenates two matrices along a specific axis

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -9,10 +9,10 @@ def cat_matrices2D(mat1, mat2, axis=0):
                 for rows in range(len(mat1))]
     cop_mat2 = [[mat2[rows][cols] for cols in range(len(mat2[0]))]
                 for rows in range(len(mat2))]
-    if axis == 0:
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
         new_mat = cop_mat1 + cop_mat2
         return(new_mat)
-    if axis == 1:
+    if axis == 1 and len(mat1) == len(mat2):
         for i in range(len(cop_mat2)):
             new_mat += [cop_mat1[i] + cop_mat2[i]]
         return(new_mat)
