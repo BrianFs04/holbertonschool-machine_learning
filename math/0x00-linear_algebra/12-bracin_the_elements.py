fd89ff20f4bbc95c9c720832125ef7eb8490d33f
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-07-15 22:37:51 +0000

    [Feature]: Performs basic operations element-wise

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,8 @@
+#!/usr/env/bin python3
+"""Function np_elementwise"""
+
+
+def np_elementwise(mat1, mat2):
+    """Performs element-wise addition, subtraction, multiplication, and division
+    in a numpy array"""
+    return((mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2))
