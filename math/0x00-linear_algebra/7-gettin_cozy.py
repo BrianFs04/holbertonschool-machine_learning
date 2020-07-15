37ebbf6e7c92d76bfcc78b79d195029ac1d140c2
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-07-15 21:21:28 +0000

    [Feature]: Concatenates two matrices along a specific axis

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Concatenates two matrices along a specific axis"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """Concatenates two matrices along a specific axis"""
+    new_mat = []
+    cop_mat1 = [[mat1[rows][cols] for cols in range(len(mat1[0]))]
+                for rows in range(len(mat1))]
+    cop_mat2 = [[mat2[rows][cols] for cols in range(len(mat2[0]))]
+                for rows in range(len(mat2))]
+    if axis == 0:
+        new_mat = cop_mat1 + cop_mat2
+        return(new_mat)
+    if axis == 1:
+        for i in range(len(cop_mat2)):
+            new_mat += [cop_mat1[i] + cop_mat2[i]]
+        return(new_mat)
+    return(None)
