36813c34e6f8eebfb4fa49a618b53a75ae0aac4e
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-07-16 02:38:31 +0000

    [Feature]: Adds two matrices element-wise

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,7 +4,7 @@
 
 def add_matrices2D(mat1, mat2):
     """Function that adds two matrices element-wise"""
-    if len(mat1) == len(mat2):
+    if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]):
         addi = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
                 for i in range(len(mat1))]
         return(addi)
