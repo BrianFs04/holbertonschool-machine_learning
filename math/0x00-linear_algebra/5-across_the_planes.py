2cd5bfd7a77787102f286d2219a2ef536b42d899
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-07-16 02:23:08 +0000

    [Fix]: Condition in which rows must be of the same size

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -15,7 +15,7 @@ def matrix_shape(matrix):
 
 def add_matrices2D(mat1, mat2):
     """Function that adds two matrices element-wise"""
-    if matrix_shape(mat1) == matrix_shape(mat2):
+    if matrix_shape(mat1) == matrix_shape(mat2) and matrix_shape(mat1[0]) == matrix_shape(mat2[0]):
         addi = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
                 for i in range(len(mat1))]
         return(addi)
