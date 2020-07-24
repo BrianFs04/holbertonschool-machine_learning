6621c2f21c6c2d891ed05af1cda28b520aac991d
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-07-24 18:54:11 +0000

    [Update]: Fix when poly is not a list

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -5,6 +5,8 @@
 def poly_derivative(poly):
     """Function that calculates the derivative of a polynomial"""
     res = []
+    if type(poly) is not list:
+        return None
     if len(poly) == 1:
         return([0])
     for i in range(1, len(poly)):
