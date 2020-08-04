eff8c19b9005ae40ed6a00ac82d66065ffc82b72
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-04 19:56:19 +0000

    [Feature]: Method that calculates one pass of gradient descent on the neuron

---
+++ supervised_learning/0x00-binary_classification/5-neuron.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""Class Neuron"""
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Constructor method"""
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """The weights vector for the neuron"""
+        return(self.__W)
+
+    @property
+    def b(self):
+        """The bias for the neuron"""
+        return(self.__b)
+
+    @property
+    def A(self):
+        """The activated output of the neuron (prediction)"""
+        return(self.__A)
+
+    def sigmoid(self, x):
+        """Returns sigmoid function"""
+        return(1/(1 + np.exp(-x)))
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron"""
+        z = np.matmul(self.__W, X) + self.__b
+        self.__A = self.sigmoid(z)
+        return(self.__A)
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model using logistic regression"""
+        m = np.shape(Y)
+        cost = -(1 / m[1]) * np.sum(Y * np.log(A) + (1 - Y) *
+                                    np.log(1.0000001 - A))
+        return(cost)
+
+    def evaluate(self, X, Y):
+        """Evaluates the neuron’s predictions"""
+        predict = self.forward_prop(X)
+        cond = np.where(predict >= 0.5, 1, 0)
+        return(cond, self.cost(Y, predict))
+    
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """Calculates one pass of gradient descent on the neuron"""
+        m = np.shape(Y)
+        loss = A - Y
+        b_dev = np.sum(loss) / m[1]
+        gradient = np.matmul(loss, X.transpose()) / m[1]
+        self.__W = self.__W - alpha * gradient
+        self.__b = self.__b - alpha * b_dev
