8871c70ec20a5e9533f2a6e6bbeb973a288a3c5d
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-06 22:22:29 +0000

    [Fix]: Range of variables in forward propagation

--- supervised_learning/0x00-binary_classification/20-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/20-deep_neural_network.py
@@ -70,6 +70,6 @@ class DeepNeuralNetwork:
 
     def evaluate(self, X, Y):
         """Evaluates the neural network’s predictions"""
-        A2, A3 = self.forward_prop(X)
+        A0, A3 = self.forward_prop(X)
         cont = np.where(self.__cache['A3'] >= 0.5, 1, 0)
         return(cont, self.cost(Y, self.__cache['A3']))
