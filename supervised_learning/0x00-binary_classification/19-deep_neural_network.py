12054866738e57af2e4d03d56fa998099c026ee1
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-06 22:01:39 +0000

    [Feature]: Calculates the cost of the model using logistic regression

---
+++ supervised_learning/0x00-binary_classification/19-deep_neural_network.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""Class DeepNeuralNetwork"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Defines a neural defines a deep neural network
+    performing binary classification"""
+
+    def __init__(self, nx, layers):
+        """Constructor method"""
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(layers) is not list or len(layers) is 0:
+            raise TypeError('layers must be a list of positive integers')
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for l in range(self.__L):
+            if type(layers[l]) is not int or layers[l] <= 0:
+                raise TypeError('layers must be a list of positive integers')
+            heetal = np.random.randn(layers[l], nx)*np.sqrt(2 / nx)
+            self.__weights['W' + str(l + 1)] = heetal
+            self.__weights['b' + str(l + 1)] = np.zeros((layers[l], 1))
+            nx = layers[l]
+
+    @property
+    def L(self):
+        """The number of layers in the neural network"""
+        return(self.__L)
+
+    @property
+    def cache(self):
+        """A dictionary to hold all intermediary values of the network"""
+        return(self.__cache)
+
+    @property
+    def weights(self):
+        """A dictionary to hold all weights and biased of the network"""
+        return(self.__weights)
+
+    def sigmoid(self, x):
+        """Returns sigmoid function"""
+        return(1/(1 + np.exp(-x)))
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neural network"""
+        self.__cache['A0'] = X
+        z1 = np.matmul(self.__weights['W' + str(1)],
+                       X) + self.__weights['b' + str(1)]
+        self.__cache['A' + str(1)] = self.sigmoid(z1)
+        for l in range(self.__L - 1):
+            w = self.__weights['W' + str(l + 2)]
+            a = self.__cache['A' + str(l + 1)]
+            b = self.__weights['b' + str(l + 2)]
+            zx = np.matmul(w, a) + b
+            self.__cache['A' + str(l + 2)] = self.sigmoid(zx)
+        return(self.__cache['A3'], self.__cache)
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model using logistic regression"""
+        m = np.shape(Y)
+        cost = -(1 / m[1]) * np.sum(Y * np.log(A) + (1 - Y) *
+                                    np.log(1.0000001 - A))
+        return(cost)
