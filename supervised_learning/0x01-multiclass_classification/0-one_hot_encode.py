a162e770afaf31bf907f673301ab8d2182f5f968
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-11 16:57:37 +0000

    [Update]: Adding a condition

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -7,6 +7,9 @@ def one_hot_encode(Y, classes):
     """Converts a numeric label vector into a one-hot matrix"""
     if type(Y) is not np.ndarray or type(classes) is not int:
         return None
+    for i in Y:
+        if i >= classes:
+            return None
     shape = (classes, np.size(Y))
     one_hot = np.zeros(shape)
     cols = np.arange(np.size(Y))
