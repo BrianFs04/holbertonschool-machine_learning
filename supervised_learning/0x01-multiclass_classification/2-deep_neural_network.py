69e1ff63d3271cf0e833a796bcc49e55c0fc18ab
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-11 20:28:26 +0000

    [Update]: Conditions

--- supervised_learning/0x01-multiclass_classification/2-deep_neural_network.py
+++ supervised_learning/0x01-multiclass_classification/2-deep_neural_network.py
@@ -133,11 +133,10 @@ class DeepNeuralNetwork:
         if ext != '.pkl':
             os.rename(filename, filename + '.pkl')
 
-    @staticmethod
     def load(filename):
         """Loads a pickled DeepNeuralNetwork object"""
         if not(os.path.exists(filename)) or not filename:
             return None
         with open(filename, 'rb') as fileObject:
             x = pickle.load(fileObject)
-        return(x)
+            return(x)
