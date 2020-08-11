01f6a1d2380b5bb744d931d944775dc668acfc5b
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-11 17:54:40 +0000

    [Fix]: pycodestyle

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -5,9 +5,11 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """Converts a one-hot matrix into a vector of labels"""
+    classes = one_hot.shape[0]
+    m = one_hot.shape[1]
     if type(one_hot) is not np.ndarray:
         return None
-    if 0 not in one_hot or 1 not in one_hot:
+    if classes > m:
         return None
     labels = np.argmax(one_hot, axis=0)
     return(labels)
