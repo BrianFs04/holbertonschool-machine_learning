7ce19e5ac7e936e68cc16d6d840b7ae7c130eca5
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-11 18:06:55 +0000

    [Feature]: Converts a one-hot matrix into a vector of labels

---
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""one_hot_decode"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """Converts a one-hot matrix into a vector of labels"""
+    if type(one_hot) is not np.ndarray:
+        return None
+    if len(one_hot.shape) is not 2:
+        return None
+    if 0 not in one_hot and 1 not in one_hot:
+        return None
+    labels = np.argmax(one_hot, axis=0)
+    return(labels)
