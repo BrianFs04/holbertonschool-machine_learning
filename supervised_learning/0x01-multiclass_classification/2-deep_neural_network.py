0fae95d05d2fc25d26d2d40c84198176316b3bb5
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-11 20:42:12 +0000

    [Update]: Conditions

--- supervised_learning/0x01-multiclass_classification/2-deep_neural_network.py
+++ supervised_learning/0x01-multiclass_classification/2-deep_neural_network.py
@@ -108,7 +108,7 @@ class DeepNeuralNetwork:
                 raise ValueError('step must be positive and <= iterations')
         x = []
         y = []
-        for i in range(iterations + 1):
+        for i in range(iterations):
             A3, self.__cache = self.forward_prop(X)
             self.gradient_descent(Y, self.__cache, alpha)
             cost = self.cost(Y, A3)
