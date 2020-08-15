4d705726218570e9cc8bcf28d9df63568eff85c8
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-15 21:03:17 +0000

    [Feature]: Creates the forward propagation graph for the neural network

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -6,7 +6,7 @@ create_layer = __import__('1-create_layer').create_layer
 
 def forward_prop(x, layer_sizes=[], activations=[]):
     """Creates the forward propagation graph for the neural network"""
-    first = create_layer(x, layer_sizes[0], activations[0])
+    A = create_layer(x, layer_sizes[0], activations[0])
     for i in range(len(layer_sizes)):
-        A = create_layer(first, layer_sizes[i], activations[i])
+        A = create_layer(A, layer_sizes[i], activations[i])
     return(A)
