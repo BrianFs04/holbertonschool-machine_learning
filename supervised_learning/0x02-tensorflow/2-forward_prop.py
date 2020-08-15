1448f5262bcc4255df3615d0f2b1cf0961a895f7
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-15 18:39:58 +0000

    [Fix]: Adding bias

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -6,6 +6,8 @@ create_layer = __import__('1-create_layer').create_layer
 
 def forward_prop(x, layer_sizes=[], activations=[]):
     """Creates the forward propagation graph for the neural network"""
-    for i in range(len(layer_sizes)):
-        a = create_layer(x, layer_sizes[i], layer_sizes[i])
-    return(a)
+    b = tf.Variable(tf.zeros(layer_sizes[i]))
+    a = create_layer(x, layer_sizes[i], activations[i])
+    with tf.name_scope("layer"):
+        add_b = tf.nn.bias_add(a, b)
+        return(add_b)
