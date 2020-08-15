3342c43b9596cee4ef146bd6ac020d8036f4eb31
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-15 14:13:45 +0000

    [Fix]: Conditions

--- supervised_learning/0x01-multiclass_classification/4-deep_neural_network.py
+++ supervised_learning/0x01-multiclass_classification/4-deep_neural_network.py
@@ -7,30 +7,30 @@ import os
 
 
 class DeepNeuralNetwork:
-        """Defines a neural defines a deep neural network
+    """Defines a neural defines a deep neural network
     performing binary classification"""
-        def __init__(self, nx, layers, activation='sig'):
-            """Constructor method"""
-            if type(nx) is not int:
-                raise TypeError('nx must be an integer')
-            if nx < 1:
-                raise ValueError('nx must be a positive integer')
-            if type(layers) is not list or len(layers) is 0:
-                raise TypeError('layers must be a list of positive integers')
-            if activation is not 'sig' and activation is not 'tanh':
-                raise ValueError("activation must be 'sig' or 'tanh'")
-            self.__L = len(layers)
-            self.__cache = {}
-            self.__weights = {}
-            self.__activation = activation
+    def __init__(self, nx, layers, activation='sig'):
+        """Constructor method"""
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(layers) is not list or len(layers) is 0:
+            raise TypeError('layers must be a list of positive integers')
+        if activation is not 'sig' and activation is not 'tanh':
+            raise ValueError("activation must be 'sig' or 'tanh'")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        self.__activation = activation
 
-            for l in range(self.__L):
-                if type(layers[l]) is not int or layers[l] <= 0:
-                    raise TypeError('layers must be a list of positive integers')
-                heetal = np.random.randn(layers[l], nx)*np.sqrt(2 / nx)
-                self.__weights['W' + str(l + 1)] = heetal
-                self.__weights['b' + str(l + 1)] = np.zeros((layers[l], 1))
-                nx = layers[l]
+        for l in range(self.__L):
+            if type(layers[l]) is not int or layers[l] <= 0:
+                raise TypeError('layers must be a list of positive integers')
+            heetal = np.random.randn(layers[l], nx)*np.sqrt(2 / nx)
+            self.__weights['W' + str(l + 1)] = heetal
+            self.__weights['b' + str(l + 1)] = np.zeros((layers[l], 1))
+            nx = layers[l]
 
     @property
     def L(self):
