a4e91fc821a95231d00a1d43ea835d67f7a42a81
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-16 04:21:37 +0000

    [Feature]: Evaluates the output of a neural network

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""evaluate"""
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+  """Evaluates the output of a neural network"""
+  saver = tf.train.import_meta_graph(save_path + '.meta')
+  with tf.Session() as sess:
+    saver.restore(sess, save_path)
+    graph = tf.get_default_graph()
+    x = tf.get_collection("x")[0]
+    y = tf.get_collection("y")[0]
+    y_pred = tf.get_collection("y_pred")[0]
+    loss = tf.get_collection("loss")[0]
+    accuracy = tf.get_collection("accuracy")[0]
+    bp, res_accu, res_loss = sess.run([y_pred, accuracy, loss],
+                                      feed_dict={x: X, y: Y})
+  return(bp, res_accu, res_loss)
