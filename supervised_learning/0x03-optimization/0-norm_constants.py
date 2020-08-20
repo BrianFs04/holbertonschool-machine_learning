bc0af8d128113027ff7f18f20fecdb663172bb97
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-20 17:23:20 +0000

    [Feature]: Calculates the normalization (standardization) constants of a matrix

--- supervised_learning/0x03-optimization/0-norm_constants.py
+++ supervised_learning/0x03-optimization/0-norm_constants.py
@@ -4,10 +4,9 @@ import numpy as np
 
 
 def normalization_constants(X):
-  """Calculates the normalization (standardization) constants of a matrix"""
-  length = X.shape[0]
-  mean = np.sum(X, axis=0) / length
-  variance = np.sum((X - mean) ** 2, axis=0) / length
-  sd = np.sqrt(variance) 
-  return(mean, sd)
-
+    """Calculates the normalization (standardization) constants of a matrix"""
+    length = X.shape[0]
+    mean = np.sum(X, axis=0) / length
+    variance = np.sum((X - mean) ** 2, axis=0) / length
+    sd = np.sqrt(variance)
+    return(mean, sd)
