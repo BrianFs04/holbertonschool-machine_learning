6c4c4dd1312dfeba0821ba7514b286fe40a96d7a
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-20 23:51:37 +0000

    [Delete]: Numpy lib

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """update_variables_RMSProp"""
-import numpy as np
 
 
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """RMSProp optimization algorithm"""
     sd = (beta2 * s) + ((1 - beta2) * grad**2)
-    var -= alpha * (grad / np.sqrt(sd + epsilon))
+    var -= alpha * (grad / ((sd + epsilon)**0.5))
     return(var, sd)
