0d05d86a5efb45814ab2b0167891b02e574a66d8
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-20 17:27:51 +0000

    [Feature]: Normalizes (standardizes) a matrix

--- supervised_learning/0x03-optimization/1-normalize.py
+++ supervised_learning/0x03-optimization/1-normalize.py
@@ -4,6 +4,6 @@ import numpy as np
 
 
 def normalize(X, m, s):
-  """Normalizes (standardizes) a matrix"""
-  Z = (X - m) / s
-  return(Z)
+    """Normalizes (standardizes) a matrix"""
+    Z = (X - m) / s
+    return(Z)
