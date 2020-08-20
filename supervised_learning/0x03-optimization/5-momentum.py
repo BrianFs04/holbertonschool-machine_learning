ef4d1c3161011a4a268b45203dd9f9886acadaca
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-20 22:48:46 +0000

    [Fix]: Pycodestyle

--- supervised_learning/0x03-optimization/5-momentum.py
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -5,6 +5,6 @@
 def update_variables_momentum(alpha, beta1, var, grad, v):
     """Updates a variable using the gradient descent
     with momentum optimization algorithm"""
-    vd = (beta1 * v) +  ((1- beta1) * grad)
+    vd = (beta1 * v) + ((1 - beta1) * grad)
     var -= alpha * vd
     return(var, vd)
