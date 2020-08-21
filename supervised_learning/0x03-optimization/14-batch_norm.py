6b719a1fba6b8506ab86732ffce9c56d95cb66c7
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-21 23:53:43 +0000

    [Fix]: Pycodestyle

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -7,11 +7,11 @@ def create_batch_norm_layer(prev, n, activation):
     """Creates a batch normalization layer for a
     neural network in tensorflow"""
     weights = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    z = tf.layers.Dense(n, activation, weights).apply(prev)
+    z = tf.layers.Dense(units=n, kernel_initializer=weights).apply(prev)
     mean, variance = tf.nn.moments(z, axes=[0])
-    gamma = tf.Variable(tf.ones([n]), True, dtype=tf.float32)
-    beta = tf.Variable(tf.zeros([n]), True, dtype=tf.float32)
-    epsilon = 1e-8
+    gamma = tf.Variable(tf.constant(1.0, shape=[n]), True)
+    beta = tf.Variable(tf.constant(0.0, shape=[n]), True)
+    epsilon = tf.constant(1e-8)
     batch_norm = tf.nn.batch_normalization(z, mean, variance,
                                            beta, gamma, epsilon)
     return(activation(batch_norm))
