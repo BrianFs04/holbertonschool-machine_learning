655a19ddc559ce847341af753ce2eb384b7b4195
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-21 01:07:18 +0000

    [Delete]: Trash

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""update_variables_RMSProp"""
-
-
-def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
-    """RMSProp optimization algorithm"""
-    sd = (beta2 * s) + ((1 - beta2) * grad**2)
-    var -= alpha * (grad / ((sd + epsilon)**0.5))
-    return(var, sd)
