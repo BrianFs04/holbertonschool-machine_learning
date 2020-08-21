4ffadb1febb675fb437f79597201d6746dfcfa76
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-21 01:13:54 +0000

    [Fix]: Pycodestyle

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -5,5 +5,5 @@
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """RMSProp optimization algorithm"""
     sd = (beta2 * s) + ((1 - beta2) * (grad**2))
-    var -=  alpha * (grad / ((sd ** 0.5) + epsilon))
+    var -= alpha * (grad / ((sd ** 0.5) + epsilon))
     return(var, sd)
