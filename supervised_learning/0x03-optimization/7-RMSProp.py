1872301f0709ff100f55b9265bd1fbd17628c1fa
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-21 01:12:16 +0000

    [Feature]: RMSProp optimization algorithm

---
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""update_variable_RMSProp"""
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """RMSProp optimization algorithm"""
+    sd = (beta2 * s) + ((1 - beta2) * (grad**2))
+    var -=  alpha * (grad / ((sd ** 0.5) + epsilon))
+    return(var, sd)
