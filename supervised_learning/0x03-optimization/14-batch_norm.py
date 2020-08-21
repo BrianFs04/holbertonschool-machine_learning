9dde5f7c6901fbd85513d1bdb9f8f9d8f9c5f8e3
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-21 23:08:25 +0000

    [Feature]: Batch normalization layer tf

---
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""crate_batch_norm_layer"""
+import tensorflow as tf
+
+
+def create_batch_norm_layer(prev, n, activation):
+    weights = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    z = tf.layers.Dense(n, activation, weights).apply(prev)
+    mean, variance = tf.nn.moments(z, axes=[0])
+    gamma = tf.Variable(tf.ones(n), True, dtype=tf.float32)
+    beta = tf.Variable(tf.zeros(n), True, dtype=tf.float32)
+    epsilon = 1e-8
+    batch_norm = tf.nn.batch_normalization(z, mean, variance,
+                                           beta, gamma, epsilon)
+    if not activation:
+        return(batch_norm)
+    return(activation(batch_norm))
