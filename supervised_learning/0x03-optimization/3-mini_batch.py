94d5100fc9ae9bc4c7847e980d788400682b06cf
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-22 01:15:55 +0000

    [Feature]: Mini-batch gradient descent

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """train_mini_batch"""
 import tensorflow as tf
+shuffle_data = __import__('2-shuffle_data').shuffle_data
 
 
 def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
