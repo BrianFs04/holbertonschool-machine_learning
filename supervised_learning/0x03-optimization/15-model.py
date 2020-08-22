041ba3c2002ee733567d44e7d117cf21e335d353
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-22 05:29:39 +0000

    [Feature]: Builds, trains, and saves a neural network model tf

--- supervised_learning/0x03-optimization/15-model.py
+++ supervised_learning/0x03-optimization/15-model.py
@@ -92,13 +92,15 @@ def model(Data_train, Data_valid, layers, activations, alpha=0.001, beta1=0.9,
     tf.add_to_collection('train_op', train_op)
 
     mini_batch = Data_train[0].shape[0] / batch_size
-    if type(mini_batch) is not int:
-        mini_batch = int(mini_batch + 1)
+    if (mini_batch).is_integer() is True:
+        mini_batch = int(mini_batch)
+    else:
+        mini_batch = int(mini_batch) + 1
 
 
+    init = tf.global_variables_initializer()
     saver = tf.train.Saver()
     with tf.Session() as sess:
-        init = tf.global_variables_initializer()
         sess.run(init)
         for i in range(epochs + 1):
             tc, ta = sess.run([loss, accuracy], feed_dict={x: Data_train[0],
