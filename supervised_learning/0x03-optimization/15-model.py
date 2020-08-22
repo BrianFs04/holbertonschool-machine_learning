b3675d86fc1b228bfe13a3d6bcee2befe4b719c8
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-22 04:23:58 +0000

    [Feature]: Builds, trains, and saves a neural network model tf

--- supervised_learning/0x03-optimization/15-model.py
+++ supervised_learning/0x03-optimization/15-model.py
@@ -74,7 +74,7 @@ def model(Data_train, Data_valid, layers, activations, alpha=0.001, beta1=0.9,
           epochs=5, save_path='/tmp/model.ckpt'):
 
     mini_batch = Data_train[0].shape[0] / batch_size
-    if (mini_batch).is_integer is False:
+    if (mini_batch).is_integer() is False:
         mini_batch = int(mini_batch + 1)
 
     x = tf.placeholder(tf.float32, shape=[None, Data_train[0].shape[1]],
