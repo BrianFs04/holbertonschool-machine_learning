0638c326b08dc7e10ae77bec88973b5921aeb3f2
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-24 21:11:09 +0000

    [Feature]: Calculates the sensitivity for each class in a confusion matrix

--- supervised_learning/0x04-error_analysis/1-sensitivity.py
+++ supervised_learning/0x04-error_analysis/1-sensitivity.py
@@ -5,6 +5,7 @@ import numpy as np
 
 def sensitivity(confusion):
     """Calculates the sensitivity for each class in a confusion matrix"""
-    tp = np.diagonal(confusion)
-    p = np.sum(confusion, axis=1)
-    return(tp / p)
+    TP = np.diagonal(confusion)
+    FN = np.sum(confusion, axis=1) - TP
+    TPR = TP / (TP + FN)
+    return(TPR)
