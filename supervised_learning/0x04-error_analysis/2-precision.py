2a20f0de42e63db9f25ae85a3c2efb36c13187b4
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-24 21:13:12 +0000

    [Feature]: Calculates the precision for each class in a confusion matrix

--- supervised_learning/0x04-error_analysis/2-precision.py
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def precision(confusion):
     """Calculates the precision for each class in a confusion matrix"""
-    tp = np.diagonal(confusion)
-    fp = np.sum(confusion, axis=0)
-    precision = tp / fp
-    return(precision)
+    TP = np.diagonal(confusion)
+    FP = np.sum(confusion, axis=0) - TP
+    PPV = TP / (TP + FP)
+    return(PPV)
