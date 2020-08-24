69a6df363be513c418b9abca599fc4c7d70c0c51
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-24 19:35:48 +0000

    [Feature]: Creates a confusion matrix

---
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""create_confusion_matrix"""
+import numpy as np
+
+def create_confusion_matrix(labels, logits):
+  """Creates a confusion matrix"""
+  c_matrix = np.matmul(labels.T, logits)
+  return(c_matrix)
