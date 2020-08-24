034f94eedb1ae3d088236b275729815ee6af8e77
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-24 21:08:54 +0000

    [Fix]: Typo

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -5,9 +5,9 @@ import numpy as np
 
 def specificity(confusion):
     """Calculates the specificity for each class in a confusion matrix"""
-    tp = np.diagonal(confusion)
-    fp = np.sum(confusion, axis=0) - tp
-    fn = np.sum(confusion, axis=1) - tp
-    tn = np.sum(confusion) - (fp + fn + tp)
-    specificity = tn / (fp + tn)
-    return(specificity)
+    TP = np.diagonal(confusion)
+    FP = np.sum(confusion, axis=0) - TP
+    FN = np.sum(confusion, axis=1) - TP
+    TN = np.sum(confusion) - (FP + FN + TP)
+    TNR = TN / (FP + TN)
+    return(TNR)
