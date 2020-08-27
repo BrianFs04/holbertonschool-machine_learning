0293dbbfa12cac8193caea923cd400e5d08bb6ee
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-27 18:46:52 +0000

    [Feature]: Cost of a neural network with L2 regularization

---
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""l2_reg_cost"""
+import tensorflow as tf
+
+
+def l2_reg_cost(cost):
+    """Calculates the cost of a neural network with L2 regularization"""
+    l2_cost = tf.losses.get_regularization_losses(scope=None)
+    return(cost * l2_cost)
