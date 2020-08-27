a430eaea8adbf5e40f1f2ae71fa7c8e4def3387e
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-27 23:13:55 +0000

    [Feature]: Dropout regularization using gradient descent

--- supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
+++ supervised_learning/0x05-regularization/5-dropout_gradient_descent.py
@@ -9,15 +9,15 @@ def dropout_gradient_descent(Y, weights, cache, alpha, keep_prob, L):
     devsz = []
     m = np.shape(Y)
     devlz = devsz.append(cache['A' + str(L)] - Y)
-    for l in range(L, 0, -1):
-        AT = cache['A' + str(l - 1)].T
-        WT = weights['W' + str(l)].T
-        A = cache['A' + str(l - 1)]
+    for ls in range(L, 0, -1):
+        AT = cache['A' + str(ls - 1)].T
+        WT = weights['W' + str(ls)].T
+        A = cache['A' + str(ls - 1)]
         devg = (1 - A**2)
-        devWx = np.matmul(devsz[L - l], AT) / m[1]
-        devbx = np.sum(devsz[L - l], axis=1, keepdims=True) / m[1]
-        if l != 1:
-            reg_drop = devg * (cache['D' + str(l - 1)] / keep_prob)
-            devzx = devsz.append(np.matmul(WT, devsz[L - l]) * reg_drop)
-        weights['W' + str(l)] -= alpha * devWx
-        weights['b' + str(l)] -= alpha * devbx
+        devWx = np.matmul(devsz[L - ls], AT) / m[1]
+        devbx = np.sum(devsz[L - ls], axis=1, keepdims=True) / m[1]
+        if ls != 1:
+            reg_drop = devg * (cache['D' + str(ls - 1)] / keep_prob)
+            devzx = devsz.append(np.matmul(WT, devsz[L - ls]) * reg_drop)
+        weights['W' + str(ls)] -= alpha * devWx
+        weights['b' + str(ls)] -= alpha * devbx
