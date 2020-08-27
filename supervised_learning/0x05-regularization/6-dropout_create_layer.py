04afc49b542638513951ac63a71834e1459d1a58
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-27 23:26:08 +0000

    [Feature]: Creates a layer of a neural network using dropout

--- supervised_learning/0x05-regularization/6-dropout_create_layer.py
+++ supervised_learning/0x05-regularization/6-dropout_create_layer.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 def dropout_create_layer(prev, n, activation, keep_prob):
     """Creates a layer of a neural network using dropout"""
     weights = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    dropout = tf.contrib.layers.l2_regularizer(keep_prob)
+    dropout = tf.layers.Dropout(keep_prob)
     layer = tf.layers.Dense(units=n, activation=activation,
                             kernel_initializer=weights,
                             kernel_regularizer=dropout).apply(prev)
