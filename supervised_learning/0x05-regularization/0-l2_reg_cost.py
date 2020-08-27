c5042cb06f868465db9509926976af1ef06b1fd3
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-08-27 17:16:12 +0000

    [Feature]: Cost of a neural network with L2 regularization

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -6,8 +6,9 @@ import numpy as np
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """Calculates the cost of a neural network with L2 regularization"""
     frobenius = 0
-    for v in weights.values():
-        frobenius += np.linalg.norm(v)
+    for i in range(1, L + 1):
+        key = 'W' + str(i)
+        frobenius += np.linalg.norm(weights[key])
     reg_term = ((lambtha / (2 * m)) * frobenius)
     l2_cost = cost + reg_term
     return(l2_cost)
