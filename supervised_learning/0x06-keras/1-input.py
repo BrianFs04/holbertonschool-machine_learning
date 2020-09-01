6c8ccae38dd736c2eaab88f2acdb3ab72e36552d
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-09-01 16:30:19 +0000

    [Feature]: Build a NN using input

--- supervised_learning/0x06-keras/1-input.py
+++ supervised_learning/0x06-keras/1-input.py
@@ -7,12 +7,11 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """Builds a neural network with the Keras library"""
     L2 = K.regularizers.l2(lambtha)
     input = K.Input(shape=(nx, ))
-    output = [K.layers.Dense(layers[0], activations[0],
-                             kernel_regularizer=L2)(input)]
-    dropout = []
+    output = K.layers.Dense(layers[0], activations[0],
+                             kernel_regularizer=L2)(input)
     for i in range(1, len(layers)):
-        dropout.append(K.layers.Dropout(1 - keep_prob)(output[i - 1]))
-        output.append(K.layers.Dense(layers[i], activations[i],
-                                     kernel_regularizer=L2)(dropout[i - 1]))
+        output = K.layers.Dropout(1 - keep_prob)(output)
+        output = K.layers.Dense(layers[i], activations[i],
+                                kernel_regularizer=L2)(output)
     model = K.Model(inputs=input, outputs=output)
     return(model)
