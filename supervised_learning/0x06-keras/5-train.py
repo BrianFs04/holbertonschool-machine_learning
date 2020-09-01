e5917840ec367bddc2cf61f328a424b579db14c0
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-09-01 16:28:47 +0000

    [Feature]: Analyzes validation data

--- supervised_learning/0x06-keras/5-train.py
+++ supervised_learning/0x06-keras/5-train.py
@@ -6,6 +6,11 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, verbose=True, shuffle=False):
     """Update the function to also analyze validation data"""
-    history = network.fit(data, labels, batch_size, epochs, verbose,
-                          validation_data=validation_data, shuffle=shuffle)
+    history = network.fit(x=data,
+                          y=labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          verbose=verbose,
+                          shuffle=shuffle,
+                          validation_data=validation_data)
     return(history)
