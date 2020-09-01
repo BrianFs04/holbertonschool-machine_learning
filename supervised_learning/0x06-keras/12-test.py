d5e53f263324679c2eba8891075de5e642656c8e
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-09-01 20:38:43 +0000

    [Feature]: Tests a neural network

---
+++ supervised_learning/0x06-keras/12-test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""test_model"""
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """Tests a neural network"""
+    loss, accuracy = network.evaluate(data, labels, verbose=verbose)
+    return(loss, accuracy)
