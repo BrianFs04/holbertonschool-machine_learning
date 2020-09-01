c723bb7f39baeb2e3f90e574856a05c4b29fee7d
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-09-01 17:07:19 +0000

    [Feature]: Train the model using early stopping

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -7,9 +7,10 @@ def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False, patience=0,
                 verbose=True, shuffle=False):
     """Update the function to train the model using early stopping"""
+    early_stop = None
     if validation_data:
         early_stop = [K.callbacks.EarlyStopping(patience=patience,
-                                                monitor='val_loss', mode='min')]
+                                                monitor='val_loss')]
     history = network.fit(x=data,
                           y=labels,
                           batch_size=batch_size,
