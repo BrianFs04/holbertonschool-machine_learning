3d8f81713fc51028432eb73ca23bac7a84bbc146
BrianFs04 <brayanflorezsanabria@gmail.com>
2020-09-16 17:39:53 +0000

    [Feature]: Transition layer

---
+++ supervised_learning/0x08-deep_cnns/6-transition_layer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""transition_layer"""
+import tensorflow.keras as K
+
+
+def transition_layer(X, nb_filters, compression):
+    """Builds a transition layer"""
+    norm1 = K.layers.BatchNormalization()(X)
+    act1 = K.layers.Activation("relu")(norm1)
+    nb_maps = X.shape[3]
+    nb_maps = int(compression * nb_maps)
+    conv1 = K.layers.Conv2D(filters=nb_maps, kernel_size=1,
+                            kernel_initializer="he_normal",
+                            padding="same")(act1)
+    avg1 = K.layers.AveragePooling2D(pool_size=2)(conv1)
+    return(avg1, nb_maps)
